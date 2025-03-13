8ea493971364e47b5baa16cc89c84bf5f6a85793
LinYungYuan <adsf555@livemail.tw>
2025-03-13 19:49:33 +0800

    upd

---
+++ deployment.tf
@@ -0,0 +1,56 @@
+
+# resource "kubernetes_deployment_v1" "nginx" {
+#   metadata {
+#     name      = "nginx"
+#     namespace = kubernetes_namespace.test["frontend"].metadata[0].namespace
+#   }
+#   spec {
+#     replicas = 2
+#     selector {
+#       match_labels = {
+#         app = "nginx"
+#       }
+#     }
+#     template {
+#       metadata {
+#         labels = { app = "nginx" }
+#       }
+#       spec {
+#         container {
+#           image = "nginx:1.14.2"
+#           name  = "nginx"
+#           port {
+#             container_port = 80
+#           }
+#         }
+#       }
+#     }
+#   }
+# }
+
+# resource "kubernetes_service_v1" "nginx" {
+#   metadata {
+#     name = "nginx"
+#   }
+#   spec {
+#     selector = {
+#       app = "nginx"
+#     }
+#     port {
+#       port        = 80
+#       target_port = 80
+#     }
+#     type = "NodePort" # or "LoadBalancer
+#   }
+# }
+
+# module "nginx-app" {
+#   source         = "./modules/image"
+#   app_name       = "nginx"
+#   image          = "nginx:1.14.2"
+#   namespace_name = kubernetes_namespace.namespace["frontend"].metadata[0].name
+# }
+
+# output "ip" {
+#   value = module.nginx-app.svc_ip
+# }
